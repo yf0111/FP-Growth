769828ef29584c8fb8980586538444a52f7f10a7
yf0111 <a0975913219@gmail.com>
2021-10-24 22:31:54 +0800

    really finish step 1-3
    before construct FPtree

--- main.cpp
+++ main.cpp
@@ -8,9 +8,11 @@
 #include <iomanip>
 #include <algorithm>
 #include <map>
+#include <typeinfo>
 // #include <iterator>
 using namespace std;
 typedef pair<string,int> mypair;
+typedef pair<int,int> item;
 
 struct FPnode{
     string name;
@@ -18,9 +20,9 @@ struct FPnode{
     FPnode* link;
     FPnode* parent;
     FPnode* child;
-    // FPnode(string name,FPnode* parent,int frequent =1):name(name),parent(parent),count(count){
-    //     link = NULL;
-    // }
+    FPnode(string name,FPnode* parent,int frequent =1):name(name),parent(parent),frequent(frequent){
+        link = NULL;
+    }
 
     bool isRoot(){
         return (parent==NULL);
@@ -38,8 +40,11 @@ class FPtree{
         FPnode* root;
         vector<int> headerTable; // maybe map??
         int minSupport; //min-support
-        map<string,int> Itemfrequent; 
-        vector<mypair> ItemfrequentVec;
+        map<string,int> Itemfrequent; // each item frequency
+        vector<mypair> ItemfrequentVec; // each item frequency (DS:vector)
+        vector<vector<int>> newTrans; // new transaction, after delete <minSupport item
+        vector<vector<item>> sortTra; // just for sorting new transaction
+        vector<vector<int>> sortedTra; //last transaction, after sorting newTrans
     public:
         // void addCount(FPnode x){
         //     x.frequent += 1;
@@ -56,6 +61,7 @@ class FPtree{
                 }
                 cout<<endl;
             }
+            // cout<<typeid(transactions[1][2]).name();
         }
         
         // traversal frequent map and show 
@@ -69,12 +75,34 @@ class FPtree{
         void showVec(){
             cout<<"showVec : "<<endl;
             for(int i=0;i<ItemfrequentVec.size();i++){
-                cout<<ItemfrequentVec[i].first<<" : "<<ItemfrequentVec[i].second<<endl;
+                cout<<ItemfrequentVec[i].first<<"\t"<<ItemfrequentVec[i].second<<endl;
             }
         }
 
         void setMinSupport(int x){
             minSupport = x;
+        }
+
+        //show NewTrans (delete < minSupport item in transaction)
+        void showNewTra(){
+            cout<<"show NewTra : "<<endl;
+            for(int i=0;i<newTrans.size();i++){
+                for(int j=0;j<newTrans[i].size();j++){
+                    cout<<newTrans[i][j];
+                }
+                cout<<endl;
+            }
+        }
+
+        //show sortedTra (sorted transaction by decreasing)
+        void showSortTra(){
+            cout<<"show sortedTra : "<<endl;
+            for(int i=0;i<sortedTra.size();i++){
+                for(int j=0;j<sortedTra[i].size();j++){
+                    cout<<sortedTra[i][j]<<"\t";
+                }
+                cout<<endl;
+            }
         }
 
         // scan the transactions-DB and count frequent of each item 
@@ -95,7 +123,6 @@ class FPtree{
         void checkFre(){
             map<string, int>::iterator iter;
             for(auto iter = Itemfrequent.cbegin();iter!=Itemfrequent.cend();){
-                cout<<"in checkfre's for!"<<endl;
                 string name = (*iter).first;
                 int count = (*iter).second;
                 if( count < minSupport ){
@@ -124,20 +151,72 @@ class FPtree{
             }
         }
 
+        // scan transaction again and sort each item in transaction 
+        // decreasing
+        
+        void generateNewTrans(vector<vector<int>> transactions){
+            // scan transaction again , delete < minSupport item
+            vector<item> rowForSort;
+            vector<int> row;
+            map<string, int>::iterator iter;
+            for(int i=0;i<transactions.size();i++){
+                for(int j=0;j<transactions[i].size();j++){
+                    stringstream s;
+                    s<<transactions[i][j];
+                    string numStr = s.str();
+                    iter =  Itemfrequent.find(numStr);
+                    if(iter != Itemfrequent.end()){
+                        row.push_back(transactions[i][j]);
+                        rowForSort.push_back(make_pair(transactions[i][j],iter->second));
+                    }
+                }
+                sortTra.push_back(rowForSort);
+                newTrans.push_back(row);
+                row.clear();
+                rowForSort.clear();
+            }
+            sortingTra();
+        }
+
+        // sorting new transaction by decreasing order
+        void sortingTra(){
+            struct newTra{
+                public:
+                    bool operator()(item a,item b){
+                        return a.second > b.second;
+                    }
+            };
+            
+            //(sortTra vector<vector<item>> ,item = <int,int>)
+            for(int i=0;i<sortTra.size();i++){
+                sort(sortTra[i].begin(),sortTra[i].end(),newTra());
+            }
+            vector<int> row;
+            for(int i=0;i<sortTra.size();i++){
+                for(int j=0;j<sortTra[i].size();j++){
+                    // cout<<sortTra[i][j].first;
+                    row.push_back(sortTra[i][j].first);
+                }
+                cout<<"\n";
+                sortedTra.push_back(row);
+                row.clear();
+            }
+            
+        }
+
         // construction fptree
         void constructionTree(){
-            // vector<mypair>::iterator it;
-            // for (it = ItemfrequentVec.begin(); it != ItemfrequentVec.end();it++){
-
-            // }
-            FPnode* curr = root;
-            for(int i=0;i<ItemfrequentVec.size();i++){
-                string name = ItemfrequentVec[i].first;
-                int count = ItemfrequentVec[i].second;
+            // FPnode* cur = &Root; //pointer cur point to root's address
+            // bool isNew = true;
+            // for(int i=0;i<ItemfrequentVec.size();i++){
+            //     string name = ItemfrequentVec[i].first;
+            //     int count = ItemfrequentVec[i].second;
                 
-            }
-            bool isnew = true;
+            // }
         }
+
+
+        
 };
 
 
@@ -188,4 +267,5 @@ int main(){
     FPtree tree;
     tree.setMinSupport(2);
     tree.scanDB(transactions);
+    tree.generateNewTrans(transactions);
 }
